6b02dbe349878969e577a9c3b2d3ddaa3312391c
wyattlaberge64 <labergew@bsdvt.org>
2019-04-11 11:26:49 -0400

    updated with special

--- story.js
+++ story.js
@@ -1,4 +1,4 @@
-// rpg-tollbooth.js
+// Dragon-Adventure.js
 
 window.onload = start;
 // Change this to match ID in your AirTable.
@@ -15,7 +15,7 @@ function getScene(record_id) {
   // Normally, you will want to keep this private.
   // This key will only be good for a couple of days.
   const key = 'key9MrzPq9PZv8yJx';
-  const base_url = 'appxZqBpFimEmyP7S'; 
+  const base_url = 'appxZqBpFimEmyP7S';  
   // Alter this to match your own AirTable base.
   // URL format is
   // https://api.airtable.com/v0/<BASE_ID>/<TABLE_NAME>/<RECORD_ID>?api_key=<YOUR_API_KEY>
@@ -28,47 +28,50 @@ function getScene(record_id) {
       // Once AJAX request returns data, we destructure
       // it and store it in variables.
       let choices = [];
-      let { title, story, delay } = data.fields;
-      // The array of strings is returned as a string by AirTable,
-      // so we need to parse it into an array.
-      if (story.includes("<br><br>")) {
-        story = story.split("<br><br>");
-      }
-      // Don't bother if the scene doesn't have any choices.
-      if (data.fields.choices) {
+      let { title, story, special} = data.fields;
+	  if (data.fields.special) {
+		switch(special) {
+		  case "M8":
+			window.location.href = "./mastermind-web/index.html";
+			break;
+		  default:
+			// code block
+		}
+	  }	  
+      	// Don't bother if the scene doesn't have any choices.
+      	else if (data.fields.choices) {
         // Collect AirTable queries for every choice into an array.
-        for (let idx = 0; idx < data.fields.choices.length; idx++) {
-          choices.push($.ajax({
-            url: `https://api.airtable.com/v0/${base_url}/choices/${data.fields.choices[idx]}?api_key=${key}`,
-            type: 'GET'
-          }));
-        }
-        // Use Promise.all() to wait until every query in the array
-        // has been returned before proceeding.
-        Promise.all(choices)
-          .then(function (data) {
-            let targetArray = [];
-
-            for (let idx = 0; idx < data.length; idx++) {
-              // Destructure the necessary fields.
-              // targets is an array
-              let { choice, targets } = data[idx].fields;
-              targetArray.push({ choice: choice, target: targets[0] });
-            }
-			console.log("Choices: "+JSON.stringify(targetArray));
-            setOptions(targetArray);
-            displayStory(story, delay);
-          })
-          .catch(function (err) {
-            console.log(err);
-          });
-      } else {
-        displayStory(story, delay);
-        // No options available.
-        setOptions({});
-      }
-    })
-    .fail(function (err) {
-      console.log(err);
-    });
+        	for (let idx = 0; idx < data.fields.choices.length; idx++) {
+          	choices.push($.ajax({
+            	url: `https://api.airtable.com/v0/${base_url}/choices/${data.fields.choices[idx]}?api_key=${key}`,
+            	type: 'GET'
+          	}));
+        	}
+        	// Use Promise.all() to wait until every query in the array
+        	// has been returned before proceeding.
+        	Promise.all(choices)
+          	.then(function (data) {
+            		let targetArray = [];
+            		for (let idx = 0; idx < data.length; idx++) {
+              		// Destructure the necessary fields.
+              		// targets is an array
+              		let { choice, targets } = data[idx].fields;
+              		targetArray.push({ choice: choice, target: targets[0] });
+            	}
+	    	displayStory(story);
+            	setOptions(targetArray);
+          	})
+          	.catch(function (err) {
+            	console.log(err);
+          	});
+     	} 
+	else {
+        	displayStory(story);
+        	// No options available.
+        	setOptions({});
+      	}
+   })
+   .fail(function (err) {
+   console.log(err);
+   });
 }
--- webhelper.js
+++ webhelper.js
@@ -1,5 +1,5 @@
+// webhelper.js
 window.onload = start;
-
 var options=[];
 var buttonElement = document.getElementById("button1");
 var currentStoryElement = document.getElementById("currentStory");
@@ -10,58 +10,27 @@ var answer;
 var textTimer;
 
 function start() {
-    setup();
+    getScene(OPENING_SCENE_ID);
 }
-
 
 function setup() {
     setOptions([{ choice: "No DB", target: "" }]);
     buttonElement.innerHTML = "What will you do?"; 
-    buttonElement.onclick = function () {
-    getScene(dropdown.value);
-    }
+    buttonElement.setAttribute("onclick", "getScene(dropdown.value)");
 }
 
 function setOptions(options) {
+    var dropdown = document.getElementById("choices");
     while (dropdown.options.length) {
         dropdown.remove(0);
     }
-    for (let i = 0; i < options.length; i++) {
+    for (var i = 0; i < options.length; i++) {
 		// This is object-oriented JavaScript (hence capital letter)
-        let option = new Option(options[i].choice, options[i].target);
-		// let option = new Option(i, options[i].target);
+        var option = new Option(options[i].choice, options[i].target);
         dropdown.options.add(option);
     }
 }
 
-function displayStory(text, delay = false, append = false) {
-    var currentStoryElement = document.getElementById("currentStory");
-    if (typeof(text) === 'string') {
-        currentStoryElement.innerHTML = text;
-    } 
-	// the following makes text reveal slowly if a delay is indicated in the database
-	else if (delay) {
-        // Disable the button to prevent making a selection before
-        // full message is delivered.
-        buttonElement.disabled = true;
-        // Keep shifting strings from the array until it is empty.
-        if (append) {
-            currentStoryElement.innerHTML += `<br /><br />${text.shift()}`;
-        } 
-		else {
-            currentStoryElement.innerHTML = text.shift();
-        }
-        if (text.length) {
-            setTimeout(function () {
-                displayStory(text, delay, true);
-            }, delay);
-        } 
-		else {
-            // Done. Re-enable button.
-            buttonElement.disabled = false;
-        }
-    } 
-	else {
-        currentStoryElement.innerHTML = text.join('<br /><br />');
-    }
+function displayStory(text) {
+    currentStoryElement.innerHTML = text;
 }
